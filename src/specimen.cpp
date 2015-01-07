6f4b90f23210a2c5c242dd5fa1b754c09345a569
kallaballa <amir@viel-zu.org>
2015-01-07 01:33:10 +0100

    punish false positives

--- src/specimen.cpp
+++ src/specimen.cpp
@@ -79,9 +79,11 @@ void Specimen::think(const std::string& candidate, bool isPass, MarkovChain& mc)
 
 	double expected = isPass ? 1.0 : -1.0;
 	fitness_ = 2.0 - std::fabs(expected - brain_->outputs_[0]);
-	if(!isPass && round(brain_->outputs_[0]) == 1.0) {
-	  fitness_ /= 2;
-	}
+	if(isPass && round(fitness_) == 2.0) {
+	  fitness_ *= 2;
+	} else if(!isPass && round(brain_->outputs_[0]) == 1.0) {
+    fitness_ /= 2;
+  }
 }
 
 Specimen Specimen::makeChild() const {
